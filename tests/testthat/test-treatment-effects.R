05a7a53f301f2c6608b9309dde99ff290caba360
bonStats <joshuajbon@gmail.com>
2021-05-26 17:31:28 +1000

    First tests for treatments added

--- tests/testthat/test-treatment-effects.R
+++ tests/testthat/test-treatment-effects.R
@@ -1,5 +1,32 @@
-context("treatment effects")
+# context("treatment effects") deprecated
 
-# with_seed(101,{
-#
-# })
+library(BART)
+library(dplyr)
+library(tidyr)
+
+# set up treatment effects values
+md_z1 <- md_z0 <- bartmodel1_modelmatrix
+md_z1[,"z"] <- 1
+md_z0[,"z"] <- 0
+
+# rows = observations, cols = MCMC samples
+check_teff_matrix <- predict(bartmodel1, newdata = md_z1) - predict(bartmodel1, newdata = md_z0)
+colnames(check_teff_matrix) <- 1:ncol(check_teff_matrix)
+check_teff_df <- check_teff_matrix %>% as_tibble() %>% mutate(.draw = 1:n()) %>%
+  pivot_longer(cols = all_of(1:ncol(check_teff_matrix)),
+               names_to = ".row",
+               values_to = "cte_check") %>%
+  mutate(.row = as.integer(.row))
+
+test_that("Treatment effects calculated correctly", {
+  td_teff <- treatment_effects(bartmodel1, treatment = "z", newdata = suhillsim1$data)
+  comp_df <- td_teff %>% full_join(check_teff_df, by = c(".row", ".draw"))
+  expect_equal(comp_df$cte, comp_df$cte_check)
+})
+
+test_that("ATE calculated correctly", {
+  td_ate <- tidy_ate(bartmodel1, treatment = "z", newdata = suhillsim1$data) %>%
+    arrange(.draw)
+  expect_equal(td_ate$ate, rowMeans(check_teff_matrix))
+})
+
