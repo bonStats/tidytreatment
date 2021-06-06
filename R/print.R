2daa0fbe9d1e7edd4d93d49c9aa5aca9e0a0d3c5
Joshua Bon <joshuajbon@gmail.com>
2021-06-06 12:23:35 +1000

    Added more BART print methods

--- R/print.R
+++ R/print.R
@@ -4,3 +4,31 @@ print.wbart <- function(x, ...){
   cat("\nBART::wbart with",length(x$yhat.train.mean),"samples\n")
   cat("components:\n", cmps)
 }
+
+#' @export
+print.pbart <- function(x, ...){
+  cmps <- paste0("\t$", names(x), collapse = "\n")
+  cat("\nBART::pbart with",length(x$yhat.train.mean),"samples\n")
+  cat("components:\n", cmps)
+}
+
+#' @export
+print.lbart <- function(x, ...){
+  cmps <- paste0("\t$", names(x), collapse = "\n")
+  cat("\nBART:lbart with",length(x$yhat.train.mean),"samples\n")
+  cat("components:\n", cmps)
+}
+
+#' @export
+print.mbart <- function(x, ...){
+  cmps <- paste0("\t$", names(x), collapse = "\n")
+  cat("\nBART::mbart with",length(x$yhat.train.mean),"samples\n")
+  cat("components:\n", cmps)
+}
+
+#' @export
+print.mbart2 <- function(x, ...){
+  cmps <- paste0("\t$", names(x), collapse = "\n")
+  cat("\nBART::mbart2 with",length(x$yhat.train.mean),"samples\n")
+  cat("components:\n", cmps)
+}
