5d706ca9f466b25f00d121100447876936a1314b
Joshua Bon <joshuajbon@gmail.com>
2023-05-18 11:26:33 +1000

    Fixed vignettes, small r cmd check changes

--- R/treatment-effects-bartCause.R
+++ R/treatment-effects-bartCause.R
@@ -6,15 +6,20 @@
 #' Assumes treated column is either a integer column of 1's (treated) and 0's (nontreated) or logical indicating treatment if TRUE.
 #'
 #' @param model A supported Bayesian model fit that can provide fits and predictions.
+#' @param treatment Not used. Treatment variable specified by \code{bartcFit} object.
+#' @param newdata Not used. extracts treatment effects already calculated by \code{bartcFit} object.
 #' @param subset Either "treated", "nontreated", or "all". Default is "all".
 #' @param common_support_method Either "sd", or "chisq". Default is unspecified, and no common support calculation is done.
 #' @param cutoff Cutoff for common support (if in use).
 #' @param ... Arguments to be passed to \code{tidybayes::fitted_draws} typically scale for \code{BART} models.
+
 #'
 #' @return A tidy data frame (tibble) with treatment effect values.
 #' @export
 #'
-treatment_effects.bartcFit <- function(model, subset = "all", common_support_method, cutoff, ...) {
+treatment_effects.bartcFit <- function(model, treatment = NULL, newdata = NULL, subset = "all", common_support_method, cutoff, ...) {
+
+  stopifnot(is.null(treatment), is.null(newdata))
 
   # update specified common support arguments
   if(missing(common_support_method)){
@@ -38,20 +43,20 @@ treatment_effects.bartcFit <- function(model, subset = "all", common_support_met
 
   # extract treatment effect
 
-  rowinfo <- tibble(.row = 1:length(refitmodel$commonSup.sub), treated = model$trt)
+  rowinfo <- dplyr::tibble(.row = 1:length(refitmodel$commonSup.sub), treated = model$trt)
   if(commonSup.rule != "none"){
-    rowinfo <- rowinfo %>% mutate(supported = refitmodel$commonSup.sub)
+    rowinfo <- rowinfo %>% dplyr::mutate(supported = refitmodel$commonSup.sub)
   }
 
   te_df <- tidy_draws(refitmodel, type = "icate", fitstage = "response", sample = "all") %>%
-    left_join(tidy_draws(refitmodel, type = "ite", fitstage = "response"),
-              by = join_by(.chain, .iteration, .draw, .row)) %>%
-    left_join(rowinfo, by = join_by(.row))
+    dplyr::left_join(tidy_draws(refitmodel, type = "ite", fitstage = "response"),
+              by = dplyr::join_by(".chain", ".iteration", ".draw", ".row")) %>%
+    dplyr::left_join(rowinfo, by = dplyr::join_by(.row))
 
   if(subset == "treated"){
-    te_df <- te_df %>% filter(treated == 1)
+    te_df <- te_df %>% dplyr::filter(!!as.symbol("treated") == 1)
   } else if (subset == "nontreated") {
-    te_df <- te_df %>% filter(treated == 0)
+    te_df <- te_df %>% dplyr::filter(!!as.symbol("treated") == 0)
   }
 
   return(te_df)
